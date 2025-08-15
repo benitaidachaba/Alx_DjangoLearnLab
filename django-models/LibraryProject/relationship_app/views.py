185e0e04d689a28de33bbab652c402357449600c
benita <benitaid2004@gmail.com>
2025-08-15 20:47:21 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
 
